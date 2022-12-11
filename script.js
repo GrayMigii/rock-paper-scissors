ce5e47c55318b665cfe386624b78e919e1c2520f
GrayMigii <abdullahkassa@gmail.com>
2022-12-11 23:36:58 +0100

    rock-paper-scissors done

---
+++ script.js
@@ -0,0 +1,57 @@
+let computerScore = 0;
+let playerScore = 0;
+
+let getComputerChoice = () => {
+    const choices = ['rock', 'paper', 'scissors'];
+    let choice = choices[Math.round(Math.random()*choices.length)];
+    return choice;
+}
+
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'draw';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++;
+        return 'player won';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        return 'player won';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++;
+        return 'player won';
+    } else if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        computerScore++;
+        return 'computer won';
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerScore++;
+        return 'computer won';
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        computerScore++;
+        return 'computer won';
+    } else {
+        return 'unknown'
+    }
+
+}
+
+let game = () => {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt('rock, paper or scissors ?')
+        console.log(playRound(playerSelection, computerSelection))
+        console.log(`Player: ${playerScore}`)
+        console.log(`Computer: ${computerScore}`)
+    }
+
+    if (playerScore === computerScore){
+        console.log('Draw')
+    }else if (playerScore > computerScore){
+        console.log('Player is the Winner')
+    }else if (computerScore > playerScore){
+        console.log('Computer is the Winner')
+    }
+
+}
+
+game();
+
